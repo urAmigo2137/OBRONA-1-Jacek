6b6ad9e2b2d120464912efa296a58a78de02a24f
urAmigo2137 <159859961+urAmigo2137@users.noreply.github.com>
2026-04-20 07:57:16 +0200

    Define Harmonogram class with time management methods
    Added a new method 'usunCzas' to remove a time entry.

---
+++ harmonogram.h
@@ -0,0 +1,30 @@
+#ifndef HARMONOGRAM_H
+#define HARMONOGRAM_H
+
+#include "Czas.h"
+
+class Harmonogram
+{
+private:
+    Czas* czasy;
+    int liczbaCzasow;
+    int pojemnosc;
+
+public:
+    Harmonogram();
+    Harmonogram(const Harmonogram& inny);
+    ~Harmonogram();
+
+    Harmonogram& operator=(const Harmonogram& inny);
+
+    void dodajCzas(Czas& nowyCzas);
+    int podajLiczbeCzasow();
+    Czas& podajCzas(int indeks);
+    void wypisz();
+
+    Harmonogram kopiujDoZakresu(Czas& zakres);
+
+    bool usunCzas(int n);   // NOWA METODA
+};
+
+#endif
